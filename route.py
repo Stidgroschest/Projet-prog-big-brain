5ba40100908993401c1fb6fa294a112922ed364d
Antoine <antoine.dubuc.1@ens.etsmtl.ca>
2024-07-07 18:32:37 -0400

    oui c moi

--- route.py
+++ route.py
@@ -8,8 +8,24 @@ def longueur():
     longueur = len(route)
     return longueur
 
-def ajouter
+#Ce programme ajouter un couple dans la route globale.
+def ajouter(couple):
+    global route
+    route.append(couple)
 
+#Ce programme retourne le dernier couple présent dans la route globale.
+def dernier_couple():
+    global route
+    couple_retourner = route[len(route)-1]
+    return couple_retourner
 
+#Ce programme détermine si un couple est contenu dans la route globale
+def couple_est_present(couple):
+    global route
+    Status = False
+    for i in range(len(route)):
+        if route[i] == couple:
+            Status = True
+    return Status
 
 
--- test_route.py
+++ test_route.py
@@ -6,7 +6,23 @@ def test_longueur():
     resultat = route.longueur()
     assert resultat == 6
     print(resultat)
+def test_ajouter():
+    global route
+    couple = (7,6)
+    resultat = route.ajouter(couple)
+    assert resultat == [(11,0),(11,1),(11,2),(10,3),(9,4),(8,5),(7,6)]
+    print(resultat)
 
+def test_dernier_couple():
+    global route
+    resultat = route.dernier_couple()
+    assert resultat == (8,5)
 
+def test_couple_est_present():
+    global route
+    resultat1 = route.couple_est_present((10,3))
+    resultat2 = route.couple_est_present((15,5))
+    assert resultat1 == True
+    assert resultat2 == False
 if __name__ == '__main__':
-    test_longueur()
+    test_couple_est_present()
